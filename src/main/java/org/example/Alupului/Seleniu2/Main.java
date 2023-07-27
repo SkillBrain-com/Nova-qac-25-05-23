b4958cf0c5de076f6b6edc5dc9a016bf0377c20e
Alupului Teodor-Iulian <93911556+Alupului@users.noreply.github.com>
2023-07-27 19:40:10 +0300

    added new mains

--- src/main/java/org/example/Alupului/Seleniu2/Main.java
+++ src/main/java/org/example/Alupului/Seleniu2/Main.java
@@ -1,4 +1,13 @@
 package org.example.Alupului.Seleniu2;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class Main {
-}
+    public static void main(String[] args) {
+        
+       }
+    }
+
+
