4ef53065c43e20777a61c2ee8598dc0001a9f461
Alupului Teodor-Iulian <93911556+Alupului@users.noreply.github.com>
2023-08-02 17:54:40 +0300

    added new mains

--- src/main/java/org/example/Alupului/Seleniu1/Herokuapp/AttributesTest/AttributesTest.java
+++ src/main/java/org/example/Alupului/Seleniu1/Herokuapp/AttributesTest/AttributesTest.java
@@ -1,9 +1,7 @@
 package org.example.Alupului.Seleniu1.Herokuapp.AttributesTest;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class AttributesTest {
@@ -13,6 +11,8 @@ public class AttributesTest {
 
 
         driver.get("https://testpages.herokuapp.com/styled/attributes-test.html");
+
+        driver.findElement(By.xpath("//div[@class='centered']/button[@class='styled-click-button']/onclick='setNewAttrib'")).click();
 
 
         driver.close();
