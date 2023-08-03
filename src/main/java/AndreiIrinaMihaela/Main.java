0329afb2a7d2c91d4d4aa7dac1ec4888f8b0077a
IrinaMihaelass <irinamihaela28@yahoo.com>
2023-08-03 20:02:12 +0300

    prima tema

--- src/main/java/AndreiIrinaMihaela/Main.java
+++ src/main/java/AndreiIrinaMihaela/Main.java
@@ -1,12 +1,12 @@
+<<<<<<< HEAD:src/main/java/AndreiIrinaMihaela/Main.java
 package AndreiIrinaMihaela;
+=======
+>>>>>>> 984de8f6425578435b704aec3fa6ba22c29af608:src/main/java/org/example/Main.java
 
 
 public class Main {
+
     public static void main(String[] args) {
-
-        System.out.println("Hello world!");
-
-
-
+        System.out.println();
     }
 }
