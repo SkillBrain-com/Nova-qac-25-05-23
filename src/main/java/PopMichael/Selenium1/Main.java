95ca155651010610e2eec0431bfbd660f2194114
michaelrobert94 <popmichael59@gmail.com>
2023-08-03 19:52:09 +0300

    sel1

--- src/main/java/PopMichael/Selenium1/Main.java
+++ src/main/java/PopMichael/Selenium1/Main.java
@@ -1,4 +1,4 @@
-package org.example.PopMichael.Selenium1;
+package PopMichael.Selenium1;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -9,12 +9,12 @@ public class Main {
         driver.get("https://demoqa.com/text-box");
 
         driver.findElement(By.id("userName")).sendKeys("Pop Michael");
-        driver.findElement(By.id("userEmail")).sendKeys("pop@gmail.com");
-        driver.findElement(By.id("currentAddress")).sendKeys("cluj");
+        driver.findElement(By.cssSelector("#userEmail")).sendKeys("pop@gmail.com");
+        driver.findElement(By.cssSelector("#currentAddress")).sendKeys("cluj");
         driver.findElement(By.id("permanentAddress")).sendKeys("clujnapoca");
         driver.findElement(By.id("submit")).click();
 
 
-        driver.quit();
+       // driver.quit();
     }
 }
